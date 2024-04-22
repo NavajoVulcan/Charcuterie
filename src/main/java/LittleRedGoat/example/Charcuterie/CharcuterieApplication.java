9fa1c5d3d6fba6e2bf1fd40e40a54169f38bc8fe
Rico Rose <r.andereck@gmail.com>
2024-04-22 16:37:33 -0500

    Start to html table

---
+++ src/main/java/LittleRedGoat/example/Charcuterie/CharcuterieApplication.java
@@ -0,0 +1,13 @@
+package LittleRedGoat.example.Charcuterie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharcuterieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharcuterieApplication.class, args);
+	}
+
+}
