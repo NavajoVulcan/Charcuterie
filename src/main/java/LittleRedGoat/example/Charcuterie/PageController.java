252a74af2cc7b327e7e5ffb117198d9130779b17
Rico Rose <r.andereck@gmail.com>
2024-05-06 20:29:35 -0500

    Start run successful then stops, try opening right pane of run window Expose error, google error
    Case 1) Error message: Failed to Configure a DataSource” Error
    
    https://www.baeldung.com/spring-boot-failed-to-configure-data-source

--- src/main/java/LittleRedGoat/example/Charcuterie/PageController.java
+++ src/main/java/LittleRedGoat/example/Charcuterie/PageController.java
@@ -3,7 +3,7 @@ package LittleRedGoat.example.Charcuterie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class PageController {
